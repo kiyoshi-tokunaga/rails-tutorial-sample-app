8f79ae6ca4408d63e5906cee002329a18d5dff4a
kiyoshi-tokunaga <ponkiyo.gokigenyou@gmail.com>
2018-02-08 23:10:28 +0900

    Add hello page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, worlkd!"
+  end
 end
